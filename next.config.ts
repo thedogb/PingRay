f172815d32882db0aaf91ca0342788f2cf6e8cfa
honghu <thedogainchina@gmail.com>
2025-02-05 16:41:51 +0800

    feat(rss): 修复了rss丢失的bug,并添加了rss meta link,帮助rsshub 检测网页rss

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import generateRSS from "@/utils/genRss";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -19,12 +20,12 @@ const nextConfig: NextConfig = {
     domains: ["cdn.jsdelivr.net"], // 添加允许的域名
   },
 
-  // webpack: (config, { isServer }) => {
-  //   if (isServer) {
-  //     generateRSS(); // 只在构建时生成
-  //   }
-  //   return config;
-  // },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      generateRSS(); // 只在构建时生成
+    }
+    return config;
+  },
 };
 
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import "@/styles/globals.css";
-import { Config } from "@/config";
+// import { Config } from "@/config";
 import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import { DefaultSeo } from "next-seo";
 import TagManager from "react-gtm-module";
+import SEO from "../../next-seo.config";
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -13,27 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <DefaultSeo
-        title={Config.SITE_NAME}
-        description={Config.SITE_DESCRIPTION}
-        canonical={Config.SITE_URL}
-        openGraph={{
-          type: "website",
-          locale: "zh_CN",
-          url: Config.SITE_URL,
-          title: Config.SITE_NAME,
-          description: Config.SITE_DESCRIPTION,
-          images: [
-            {
-              url: Config.SITE_URL + Config.SITE_IMAGE,
-              width: 600,
-              height: 600,
-              alt: Config.SITE_NAME,
-            },
-          ],
-          site_name: Config.SITE_NAME,
-        }}
-      />
+      <DefaultSeo {...SEO}/>
       <Component {...pageProps} />
     </>
   );
